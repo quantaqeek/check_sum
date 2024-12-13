3eac22ea49776c5d501eca13dc1deb0cef28137e
Bettenoch <bettenoch254@gmail.com>
2024-12-14 00:27:58 +0300

    ADD: project components

---
+++ src/app/data/index.tsx
@@ -0,0 +1,341 @@
+export const achievements = [
+  {
+    id: 1,
+    title: "Google Reviews",
+    scale: 5,
+    bg: "#424242",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-12"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m4.5 12.75 6 6 9-13.5"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    title: "Happy Clients",
+    scale: 30,
+    bg: "#FFFDB8",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-12"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 3,
+    title: "Referrals",
+    scale: 60,
+    bg: "#f6f6f6",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-12"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m8.99 14.993 6-6m6 3.001c0 1.268-.63 2.39-1.593 3.069a3.746 3.746 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043 3.745 3.745 0 0 1-3.068 1.593c-1.268 0-2.39-.63-3.068-1.593a3.745 3.745 0 0 1-3.296-1.043 3.746 3.746 0 0 1-1.043-3.297 3.746 3.746 0 0 1-1.593-3.068c0-1.268.63-2.39 1.593-3.068a3.746 3.746 0 0 1 1.043-3.297 3.745 3.745 0 0 1 3.296-1.042 3.745 3.745 0 0 1 3.068-1.594c1.268 0 2.39.63 3.068 1.593a3.745 3.745 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.297 3.746 3.746 0 0 1 1.593 3.068ZM9.74 9.743h.008v.007H9.74v-.007Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+        />
+      </svg>
+    ),
+  },
+];
+
+export const services = [
+  {
+    name: "WEB",
+    notes:
+      "I create fast, secure, and visually engaging websites that provide a seamless experience for your users, helping you connect with customers and grow your online presence. By leveraging modern frameworks and best practices, I build scalable solutions that adapt to your business needs.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "MOBILE",
+    notes:
+      "I design responsive and high-performing mobile apps that make it easy for your audience to engage with your business from anywhere. Whether on iOS or Android, my apps are crafted to deliver smooth user experiences and drive customer engagement.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "PRODUCT",
+    notes:
+      "From concept to execution, I bring your ideas to life with intuitive, user-centered designs that align with your brands vision. My design process prioritizes both aesthetics and functionality, ensuring that your product not only looks great but also meets user needs effectively.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+        />
+      </svg>
+    ),
+  },
+];
+
+export const testimonies = [
+  {
+    id: 1,
+    name: "James Kariuki",
+    type: "Customer Review",
+    service: "Geospatial Analysis",
+    title: "CTO, GeoSense Ltd",
+    phoneNumber: "0712345678",
+    image: "/assets/avatars/Gavatar1.jpeg",
+    social: "https://www.linkedin.com/",
+    descp:
+      "Enochs innovative solutions in geospatial analytics have transformed our data insights, leading to more accurate and efficient decision-making within our team.",
+  },
+  {
+    id: 2,
+    name: "Grace Njeri",
+    type: "Colleague Review",
+    service: "Web Development",
+    title: "Frontend Developer, TechLabs",
+    phoneNumber: "0798765432",
+    image: "/assets/avatars/Lavatar1.jpeg",
+    social: "https://www.github.com/",
+    descp:
+      "I had the pleasure of collaborating with Enoch on a web development project where his expertise in React and clean code practices stood out. He was instrumental in meeting tight deadlines without compromising quality.",
+  },
+  {
+    id: 3,
+    name: "Alex Kimani",
+    type: "Client Review",
+    service: "E-commerce Platform",
+    title: "Founder, QuickCart",
+    phoneNumber: "0723456789",
+    image:"/assets/avatars/Gavatar3.jpeg",
+    social: "https://www.twitter.com/",
+    descp:
+      "Enoch designed an intuitive e-commerce platform that streamlined our checkout process, resulting in a 30% increase in sales conversions. His attention to user experience is exceptional.",
+  },
+  {
+    id: 4,
+    name: "Lydia Otieno",
+    type: "Project Partner Review",
+    service: "Mobile App Development",
+    title: "Project Manager, HealthTech Innovations",
+    phoneNumber: "0734567890",
+    image: "/assets/avatars/Lavatar2.jpeg",
+    social: "https://www.facebook.com/",
+    descp:
+      "I worked with Enoch on a health app development project, and his expertise in mobile app development was a game-changer. He was thorough, proactive, and always willing to tackle complex technical challenges with a positive attitude.",
+  },
+  {
+    id: 5,
+    name: "Victor Bett",
+    type: "Client Review",
+    service: "E-commerce Platform",
+    title: "Founder, QuickCart",
+    phoneNumber: "0723456789",
+    image:"/assets/avatars/Gavatar5.jpeg",
+    social: "https://www.twitter.com/",
+    descp:
+      "Enoch designed an intuitive e-commerce platform that streamlined our checkout process, resulting in a 30% increase in sales conversions. His attention to user experience is exceptional.",
+  },
+  {
+    id: 6,
+    name: "Emmanuel Rotich",
+    type: "Project Partner Review",
+    service: "AI Dashboard",
+    title: "Project Manager, HealthTech Innovations",
+    phoneNumber: "0734567890",
+    image: "/assets/avatars/Gavatar2.jpeg",
+    social: "https://www.facebook.com/",
+    descp:
+      "I worked with Enoch on a health app development project, and his expertise in mobile app development was a game-changer. He was thorough, proactive, and always willing to tackle complex technical challenges with a positive attitude.",
+  },
+  {
+    id: 7,
+    name: "Cynthia Kirimi",
+    type: "Colleague Review",
+    service: "Web Development",
+    title: "Gis Developer, Ramani",
+    phoneNumber: "0798765432",
+    image: "/assets/avatars/Lavatar3.jpeg",
+    social: "https://www.github.com/",
+    descp:
+      "I had the pleasure of collaborating with Bett on a remote sensing project involving large-scale satellite data processing. Their approach to problem-solving and their mastery of Python for data analysis were invaluable to our success. Beyond their technical skills, they were always approachable and willing to mentor others on the team, making them an asset to any project.",
+  },
+  {
+    id: 8,
+    name: "Deborah Koech",
+    type: "Colleague Review",
+    service: "Machine Learning Model",
+    title: "Clinical Officer, Agha Khan",
+    phoneNumber: "0798765432",
+    image: "/assets/avatars/Lavatar4.jpeg",
+    social: "https://www.github.com/",
+    descp:
+      "Enoch developed a custom web application for our environmental monitoring project that seamlessly integrated GIS and AI/ML capabilities. Their solution allowed us to predict land use changes with remarkable accuracy, which significantly enhanced our decision-making. Their professionalism and ability to translate complex requirements into a user-friendly interface were outstanding",
+  },
+];
+
+export const Stacks = [
+  {
+    id: 1,
+    name: "Python",
+    img: "/images/stacks/python.png"
+  },
+  {
+    id: 2,
+    name: "FastApi",
+    img: "/images/stacks/FastAPI.png"
+  },
+  {
+    id: 3,
+    name: "Django",
+    img: "/images/stacks/django.png"
+  },
+  {
+    id: 4,
+    name: "JavaScript",
+    img: "/images/stacks/javascript.png"
+  },
+  {
+    id: 5,
+    name: "React",
+    img: "/images/stacks/react.png"
+  },
+  {
+    id: 6,
+    name: "Typescript",
+     img: "/images/stacks/typescript.png"
+  },
+  {
+    id: 7,
+    name: "Php",
+    img: "/images/stacks/php.svg"
+  },
+  {
+    id: 8,
+    name: "Ruby",
+    img: "/images/stacks/ruby.svg"
+  },
+  {
+    id: 9,
+    name: "Ruby on Rails",
+    img: "/images/stacks/ror.png"
+  },
+  {
+    id: 10,
+    name: "Mobile",
+    img: "/images/stacks/flutter.png"
+  },
+
+  
+]
+
+
+export const Projects = [
+  {
+    id: 1,
+    name: "RAGS",
+    descp: "PDF CHAT APPLICATION",
+    image: "https://img.freepik.com/free-photo/black-swan-cygnus-atratus-illustrated-by-elizabeth-gould_53876-65218.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["RAGS", "LLMS", "PYTHON", "LLAMAINDEX", "OPENAI"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+  {
+    id: 2,
+    name: "FETCHIT BOT",
+    descp: "PDF CHAT APPLICATION",
+    image: "https://img.freepik.com/free-photo/bright-pop-landscape-design_23-2149213430.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["RAGS", "LLMS", "PYTHON", "Botmake", "Langchain"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+  {
+    id: 3,
+    name: "CDIS",
+    descp: "County Information Management System",
+    image: "https://img.freepik.com/premium-photo/friends-painting-colorful-mural-their-creativity-friendship-intertwining_741910-44795.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["Python", "JavaScript", "GEE", "ArcGIS", "GeoServer"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+  {
+    id: 4,
+    name: "Doctor Apppointment Booking",
+    descp: "Doctor Appointment Application",
+    image: "https://img.freepik.com/premium-photo/frontal-view-woman-with-vr-glasses_796128-85.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["Ruby", "Ruby on Rails", "Tailwind", "Material UI"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+  {
+    id: 5,
+    name: "NEUROPTIK AUTOMATIONS",
+    descp: "AI DASHBOARD",
+    image: "https://img.freepik.com/premium-photo/double-exposure-image-engineer-safety-helmet-with-city_31965-92763.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["RAGS", "LLMS", "PYTHON", "Make", "n8n"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+  {
+    id: 6,
+    name: "PHOTO STUDIO",
+    descp: "PDF CHAT APPLICATION",
+    image: "https://img.freepik.com/premium-photo/colorful-light-bulb-generative-ai_220873-17777.jpg?uid=R169576943&ga=GA1.1.134588729.1722247423&semt=ais_hybrid",
+    tags: ["Python", "React", "FastAPi", "Typescript", "Tailwind Css"],
+    link: "https://github.com/Bettenoch?tab=repositories"
+  },
+
+]
