2dfa360e89420459f1f97ae0579427db0fa4ad4d
Bettenoch <bettenoch254@gmail.com>
2024-12-14 00:28:05 +0300

    ADD: project components

---
+++ src/app/services/page.tsx
@@ -0,0 +1,12 @@
+import ServicesHero from '@/components/services/serviceHero'
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='flex flex-col w-full items-center min-h-screen font-roboto'>
+      <ServicesHero/>
+    </div>
+  )
+}
+
+export default Services
