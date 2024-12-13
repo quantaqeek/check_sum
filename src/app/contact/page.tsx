a0946cb3daeb430d6ec4cb5c3298bd78350589c5
Bettenoch <bettenoch254@gmail.com>
2024-12-14 00:27:56 +0300

    ADD: project components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import ContactForm from "@/components/contacts/contactForm";
+import ContactsHero from "@/components/contacts/ContactsHero";
+
+import React from "react";
+
+const Contacts = () => {
+  return (
+<div className="flex flex-col w-full">
+  <ContactsHero/>
+<ContactForm/>
+</div>
+  );
+};
+
+export default Contacts;
